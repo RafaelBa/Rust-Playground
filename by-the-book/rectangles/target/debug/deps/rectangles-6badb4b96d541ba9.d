b31dfabffa42adccaf691446424907ee34bfe2f5
Rafael <11751298+RafaelBa@users.noreply.github.com>
2018-06-04 17:29:41 +0200

    Added .gitignore to chapter 5.2

--- by-the-book/rectangles/target/debug/deps/rectangles-6badb4b96d541ba9.d
+++
@@ -1,5 +0,0 @@
-/Users/rafael/Pets/rust-playground/by-the-book/rectangles/target/debug/deps/rectangles-6badb4b96d541ba9: src/main.rs
-
-/Users/rafael/Pets/rust-playground/by-the-book/rectangles/target/debug/deps/rectangles-6badb4b96d541ba9.d: src/main.rs
-
-src/main.rs:
--- by-the-book/rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-/Users/rafael/Pets/rust-playground/by-the-book/rectangles/target/debug/rectangles.dSYM: /Users/rafael/Pets/rust-playground/by-the-book/rectangles/src/main.rs
