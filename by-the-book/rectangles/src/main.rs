dac49a1853987f2e758e6aaa3938a7c80f768e59
Rafael <11751298+RafaelBa@users.noreply.github.com>
2018-06-22 19:34:45 +0200

    by-the-book: completed chapter 5 on structs with rectangles

--- by-the-book/rectangles/src/main.rs
+++ by-the-book/rectangles/src/main.rs
@@ -4,8 +4,31 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width &&
+            self.height >= other.height
+    }
+}
+
+impl Rectangle {
+    fn i_am_in_a_seperate_impl(&self) -> String {
+        format!("I am coming from another block ({:#?})", self)
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
     println!(
         "The area of the reactangle is {} square pixels",
@@ -17,6 +40,33 @@ fn main() {
                                         //   width: 30,
                                         //   height: 50
                                         // }
+
+    println!(
+        "Calling area method of rect1: {}",
+        rect1.area()
+    );
+
+    println!(
+        "Can rect1 hold rect2? {}",
+        rect1.can_hold(&rect2)
+    );
+
+    println!(
+        "Can rect1 hold rect3? {}",
+        rect1.can_hold(&rect3)
+    );
+
+    let square1 = Rectangle::square(400);
+
+    println!(
+        "Square via 'Rectangle::square': {:#?}",
+        square1
+    );
+
+    println!(
+        "Calling method from another impl block: {}",
+        square1.i_am_in_a_seperate_impl()
+    );
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
