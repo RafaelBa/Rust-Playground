8dd9f93d26d194caf519172091089fbff6509776
Rafael <11751298+RafaelBa@users.noreply.github.com>
2018-07-01 21:26:25 +0200

    by-the-book: Added 'use' example from chapte 7.3 into communicator

--- by-the-book/communicator/src/lib.rs
+++ by-the-book/communicator/src/lib.rs
@@ -4,8 +4,11 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;   // `super` references the current parent object
+    // use ::client;     // `::` at the first position means we want to start at root
+    
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        client::connect();
     }
 }
